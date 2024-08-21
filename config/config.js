403f7780e0a5d7abb821f6972f8579403fd2422f
saveearth <saveearthdev>
2024-08-20 20:40:31 -0500

    feat: mongodb connent

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://127.0.0.1:27017/mongo_test_db',
+    secretkey: 'secret'
+}
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+
+
+const db = require('./config/config').mongoURI;
+mongoose
+    .connect(db)
+    .then(() => console.log(`mongodb connected on ${db}`))
+    .catch((err) => console.log(err))
 
 app.get('/', function (req, res) {
   res.send('Hello World')
